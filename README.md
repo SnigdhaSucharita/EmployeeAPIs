3af80f336355d1d219adab06dac753f67fb594a6
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2024-09-16 10:45:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI/CD With Github Actions
+
+> Learning how to create CICD pipelines with Github actions. This repository contains APIs to fetch employees and employee by id.
---
+++ index.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const express = require('express');
+const { getAllEmployees, getEmployeeById } = require("./controllers/index.js");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/employees", async (req, res) => {
+  const employees = getAllEmployees();
+  res.json({ employees });
+});
+
+app.get("/employees/details/:id", async(req, res) => {
+  let employee = getEmployeeById(parseInt(req.params.id));
+  res.json({ employee });
+});
+
+module.exports = { app };
